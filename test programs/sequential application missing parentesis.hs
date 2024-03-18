564e2fcfe558d402874126aa5e6dee795baa144f
sijmen_v_b <sijmenvanbommel@gmail.com>
2024-03-18 17:15:48 +0100

    added test programs

---
+++ test programs/sequential application missing parentesis.hs
@@ -0,0 +1,10 @@
+fun1 :: Int -> Bool
+fun1 x = True
+
+fun2 :: Char -> Int
+fun2 x = 42
+
+val :: Char
+val = 'c'
+
+expression = fun1 fun2 val
---
+++ test programs/simple.hs
@@ -0,0 +1,7 @@
+fun1 :: Int -> Bool
+fun1 x = True
+
+val :: Int
+val = 42
+
+expression = fun1 val
