74f5d5a6890314b8e496030f8aafdd91c5ea5be8
sijmen_v_b <sijmenvanbommel@gmail.com>
2024-07-12 00:55:39 +0200

    current test program.

--- test programs/test.hs
+++ test programs/test.hs
@@ -1,1 +1,19 @@
-34534 + 324
+fun1 x = (True) case
+val :: Int
+val =
+  42
+
+expression =
+  fun
+    val
+
+expression2 =
+  fun
+    fun2
+      val
+
+expression3 = let x = 345 in plus 4 x
+
+fun3 = 34
+
+fun x = (\y -> add5 y ) 12
